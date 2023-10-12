57197b16bb0adfee9c0feaed6629b00967c93a0e
HaozheLiao <hlliao@student.unimelb.edu.au>
2023-10-12 21:53:00 +1100

    fix some bug

--- assets/js/morris-conf.js
+++ assets/js/morris-conf.js
@@ -5,32 +5,32 @@ var Script = function () {
     $(function () {
       // data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type
       var tax_data = [
-           {"period": "2011 Q3", "licensed": 3407, "sorned": 660},
-           {"period": "2011 Q2", "licensed": 3351, "sorned": 629},
-           {"period": "2011 Q1", "licensed": 3269, "sorned": 618},
-           {"period": "2010 Q4", "licensed": 3246, "sorned": 661},
-           {"period": "2009 Q4", "licensed": 3171, "sorned": 676},
-           {"period": "2008 Q4", "licensed": 3155, "sorned": 681},
-           {"period": "2007 Q4", "licensed": 3226, "sorned": 620},
-           {"period": "2006 Q4", "licensed": 3245, "sorned": null},
-           {"period": "2005 Q4", "licensed": 3289, "sorned": null}
+           {"period": "2022 Q3", "Sold": 3407, "Unsold": 660},
+           {"period": "2022 Q2", "Sold": 3351, "Unsold": 629},
+           {"period": "2022 Q1", "Sold": 3269, "Unsold": 618},
+           {"period": "2021 Q4", "Sold": 3246, "Unsold": 661},
+           {"period": "2020 Q4", "Sold": 3171, "Unsold": 676},
+           {"period": "2019 Q4", "Sold": 3155, "Unsold": 681},
+           {"period": "2018 Q4", "Sold": 3226, "Unsold": 620},
+           {"period": "2017 Q4", "Sold": 3245, "Unsold": null},
+           {"period": "2016 Q4", "Sold": 3289, "Unsold": null}
       ];
       Morris.Line({
         element: 'hero-graph',
         data: tax_data,
         xkey: 'period',
-        ykeys: ['licensed', 'sorned'],
-        labels: ['Licensed', 'Off the road'],
+        ykeys: ['Sold', 'Unsold'],
+        labels: ['Sold', 'Unsold'],
         lineColors:['#4ECDC4','#ed5565']
       });
 
       Morris.Donut({
         element: 'hero-donut',
         data: [
-          {label: 'Jam', value: 25 },
-          {label: 'Frosted', value: 40 },
-          {label: 'Custard', value: 25 },
-          {label: 'Sugar', value: 10 }
+          {label: 'Carpet 1', value: 25 },
+          {label: 'Carpet 2', value: 40 },
+          {label: 'Carpet 3', value: 25 },
+          {label: 'Carpet 4', value: 10 }
         ],
           colors: ['#3498db', '#2980b9', '#34495e'],
         formatter: function (y) { return y + "%" }
@@ -39,21 +39,21 @@ var Script = function () {
       Morris.Area({
         element: 'hero-area',
         data: [
-          {period: '2010 Q1', iphone: 2666, ipad: null, itouch: 2647},
-          {period: '2010 Q2', iphone: 2778, ipad: 2294, itouch: 2441},
-          {period: '2010 Q3', iphone: 4912, ipad: 1969, itouch: 2501},
-          {period: '2010 Q4', iphone: 3767, ipad: 3597, itouch: 5689},
-          {period: '2011 Q1', iphone: 6810, ipad: 1914, itouch: 2293},
-          {period: '2011 Q2', iphone: 5670, ipad: 4293, itouch: 1881},
-          {period: '2011 Q3', iphone: 4820, ipad: 3795, itouch: 1588},
-          {period: '2011 Q4', iphone: 15073, ipad: 5967, itouch: 5175},
-          {period: '2012 Q1', iphone: 10687, ipad: 4460, itouch: 2028},
-          {period: '2012 Q2', iphone: 8432, ipad: 5713, itouch: 1791}
+          {period: '2021 Q1', Carpet1: 2666, Carpet2: null, Carpet3: 2647},
+          {period: '2021 Q2', Carpet1: 2778, Carpet2: 2294, Carpet3: 2441},
+          {period: '2021 Q3', Carpet1: 4912, Carpet2: 1969, Carpet3: 2501},
+          {period: '2021 Q4', Carpet1: 3767, Carpet2: 3597, Carpet3: 5689},
+          {period: '2022 Q1', Carpet1: 6810, Carpet2: 1914, Carpet3: 2293},
+          {period: '2022 Q2', Carpet1: 5670, Carpet2: 4293, Carpet3: 1881},
+          {period: '2022 Q3', Carpet1: 4820, Carpet2: 3795, Carpet3: 1588},
+          {period: '2022 Q4', Carpet1: 15073, Carpet2: 5967, Carpet3: 5175},
+          {period: '2023 Q1', Carpet1: 10687, Carpet2: 4460, Carpet3: 2028},
+          {period: '2023 Q2', Carpet1: 8432, Carpet2: 5713, Carpet3: 1791}
         ],
 
           xkey: 'period',
-          ykeys: ['iphone', 'ipad', 'itouch'],
-          labels: ['iPhone', 'iPad', 'iPod Touch'],
+          ykeys: ['Carpet1', 'Carpet2', 'Carpet3'],
+          labels: ['Carpet1', 'Carpet2', 'Carpet3'],
           hideHover: 'auto',
           lineWidth: 1,
           pointSize: 5,
@@ -65,16 +65,16 @@ var Script = function () {
       Morris.Bar({
         element: 'hero-bar',
         data: [
-          {device: 'iPhone', geekbench: 136},
-          {device: 'iPhone 3G', geekbench: 137},
-          {device: 'iPhone 3GS', geekbench: 275},
-          {device: 'iPhone 4', geekbench: 380},
-          {device: 'iPhone 4S', geekbench: 655},
-          {device: 'iPhone 5', geekbench: 1571}
+          {device: 'Carpet', Total: 136},
+          {device: 'Carpet 2', Total: 137},
+          {device: 'Carpet 3', Total: 275},
+          {device: 'Carpet 4', Total: 380},
+          {device: 'Carpet 5', Total: 655},
+          {device: 'Carpet 6', Total: 1571}
         ],
         xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
+        ykeys: ['Total'],
+        labels: ['Total'],
         barRatio: 0.4,
         xLabelAngle: 35,
         hideHover: 'auto',
@@ -87,11 +87,11 @@ var Script = function () {
         ykeys: ['value'],
         labels: ['Value'],
         data: [
-          {year: '2008', value: 20},
-          {year: '2009', value: 10},
-          {year: '2010', value: 5},
-          {year: '2011', value: 5},
-          {year: '2012', value: 20}
+          {year: '2018', value: 20},
+          {year: '2019', value: 10},
+          {year: '2020', value: 5},
+          {year: '2021', value: 5},
+          {year: '2022', value: 20}
         ]
       });
 
